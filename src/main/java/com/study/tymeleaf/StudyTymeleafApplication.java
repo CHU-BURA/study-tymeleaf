94b54548ef51f4259e9f81284707a8b6c3a2b2fd
CHU-BURA <>
2018-03-10 18:46:40 +0900

    準備 - spring boot

---
+++ src/main/java/com/study/tymeleaf/StudyTymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.study.tymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTymeleafApplication.class, args);
+	}
+}
